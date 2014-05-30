93a1507ec30db9f2259e653a2d15e61d2afb8dba
Mike Mannix <mmannix@medplus.com>
2014-05-30 19:54:06 -0400

    Set up storyboards and universal view controller

---
+++ TopMovies/MovieListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MovieListViewController.h
+//  TopMovies
+//
+//  Created by Mike Mannix on 5/30/14.
+//  Copyright (c) 2014 Mike Mannix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieListViewController : UICollectionViewController
+
+@end
