f4dc7cde807ba4d553531f73ab26c9f43ee917b9
Mike Mannix <mmannix@medplus.com>
2014-05-30 19:05:55 -0400

    Initial Commit

---
+++ TopMovies/MMAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  MMAppDelegate.h
+//  TopMovies
+//
+//  Created by Mike Mannix on 5/30/14.
+//  Copyright (c) 2014 Mike Mannix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TopMovies/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TopMovies
+//
+//  Created by Mike Mannix on 5/30/14.
+//  Copyright (c) 2014 Mike Mannix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMAppDelegate class]));
+    }
+}
