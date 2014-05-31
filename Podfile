f1abd40396a79f54a62240c6f29b4db5f0808d6d
Mike Mannix <mmannix@medplus.com>
2014-05-30 20:00:23 -0400

    Set up CocoaPods and included AFNetworking and MagicalRecord

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '7.0'
+
+target 'TopMovies' do
+	pod "AFNetworking", '~> 2.0'
+	pod 'MagicalRecord', '~> 2.2'
+end
---
+++ Pods/Pods-TopMovies-MagicalRecord-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TopMovies_MagicalRecord : NSObject
+@end
+@implementation PodsDummy_Pods_TopMovies_MagicalRecord
+@end
